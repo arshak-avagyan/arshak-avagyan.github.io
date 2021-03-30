cc91125ee3b768062d1c579cfaaeb7f0627abbc6
arshak-avagyan <arshaktumo@gmail.com>
2021-03-30 21:36:24 +0400

    First

--- adbanner/app.js
+++ adbanner/app.js
@@ -8,14 +8,20 @@ videoBanner.addEventListener('mouseenter', () => {
     videoBanner.style.transform = "translateZ(100px)";
     videoBanner.style.boxShadow = "0 4px 18px 0 rgba(6,7,11), 0 6px 30px 0 rgba(0, 0, 0, 0.19)";
     logo.style.transform = "translateZ(20px)";
-    shadow.forEach((e)=>{e.style.textShadow = "0px 10px 10px #3F4A75"});
+    shadow.forEach((e)=>{
+        e.style.textShadow = "0px 10px 10px #3F4A75";
+        e.style.transform = "translateZ(30px)";
+    });
 })
 
 videoBanner.addEventListener('mouseleave', () => {
     videoBanner.style.transform = "translateZ(0px)";
     videoBanner.style.boxShadow = "0 4px 18px 0 rgba(0, 0, 0, 0.2), 0 6px 30px 0 rgba(0, 0, 0, 0.19)";
     logo.style.transform = "translateZ(0px)";
-    shadow.forEach((e)=>{e.style.textShadow = "none"});
+    shadow.forEach((e)=>{
+        e.style.textShadow = "none";
+        e.style.transform = "translateZ(0px)";
+    });
 })
 
 button.addEventListener('mouseenter', () => {
